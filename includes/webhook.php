aecfeacb3cb329014f1d29ffa6c1037d54779048
Bastiaan Dewaele <bastiaan@storychief.io>
2024-05-14 15:23:46 +0200

    Move story status to meta

--- includes/webhook.php
+++ includes/webhook.php
@@ -2,46 +2,51 @@
 
 namespace Storychief\Webhook;
 
-use WP_REST_Request;
 use WP_Error;
+use WP_REST_Request;
 
 use function Storychief\Settings\get_sc_option;
 
-function disable_cron() {
+function disable_cron()
+{
     if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], '/wp-json/storychief/') !== false) {
-        if ( ! defined( 'DISABLE_WP_CRON' ) ) {
-            define( 'DISABLE_WP_CRON', true );
+        if (! defined('DISABLE_WP_CRON')) {
+            define('DISABLE_WP_CRON', true);
         }
 
         remove_action('init', 'wp_cron');
         remove_action('wp_loaded', '_wp_cron');
     }
 }
-add_action('plugins_loaded', __NAMESPACE__ . '\disable_cron', 0, 20);
+add_action('plugins_loaded', __NAMESPACE__.'\disable_cron', 0, 20);
 
-function register_routes() {
-    register_rest_route('storychief', 'webhook', array(
-        'methods'  => 'POST',
-        'callback' =>  __NAMESPACE__ . '\handle',
+function register_routes()
+{
+    register_rest_route('storychief', 'webhook', [
+        'methods' => 'POST',
+        'callback' => __NAMESPACE__.'\handle',
         'permission_callback' => '__return_true',
-    ));
+    ]);
 }
-add_action('rest_api_init', __NAMESPACE__ . '\register_routes');
-
+add_action('rest_api_init', __NAMESPACE__.'\register_routes');
 
 /**
  * The Main webhook function, orchestrates the requested event to its corresponding function.
  *
- * @param WP_REST_Request $request
  * @return mixed
  */
-function handle(WP_REST_Request $request) {
+function handle(WP_REST_Request $request)
+{
     storychief_debug_mode();
 
     $payload = json_decode($request->get_body(), true);
 
-    if (!\Storychief\Tools\validMac($payload)) return new WP_Error('invalid_mac', 'The Mac is invalid', array('status' => 400));
-    if (!isset($payload['meta']['event'])) return new WP_Error('no_event_type', 'The event is not set', array('status' => 400));
+    if (! \Storychief\Tools\validMac($payload)) {
+        return new WP_Error('invalid_mac', 'The Mac is invalid', ['status' => 400]);
+    }
+    if (! isset($payload['meta']['event'])) {
+        return new WP_Error('no_event_type', 'The event is not set', ['status' => 400]);
+    }
 
     $payload = apply_filters('storychief_before_handle_filter', $payload);
 
@@ -67,11 +72,15 @@ function handle(WP_REST_Request $request) {
             break;
     }
 
-    if (is_wp_error($response)) return $response;
+    if (is_wp_error($response)) {
+        return $response;
+    }
 
     $response = apply_filters('storychief_alter_response', $response);
 
-    if (!is_null($response)) $response  = \Storychief\Tools\appendMac($response);
+    if (! is_null($response)) {
+        $response = \Storychief\Tools\appendMac($response);
+    }
 
     return rest_ensure_response($response);
 }
@@ -79,13 +88,14 @@ function handle(WP_REST_Request $request) {
 /**
  * Handle a publish webhook call
  *
- * @param $payload
  * @return array
  *
  * @link https://developers.storychief.io/#fb73b0fc-39f6-4c2e-b231-6687a2646287
  */
-function handlePublish($payload) {
+function handlePublish($payload)
+{
     $story = $payload['data'];
+    $meta = $payload['meta'];
 
     // Before publish action
     do_action('storychief_before_publish_action', array_merge([], $story));
@@ -93,7 +103,7 @@ function handlePublish($payload) {
     $is_test_mode = (bool) get_sc_option('test_mode');
     $is_draft = $is_test_mode;
 
-    if (!$is_test_mode && isset($story['status']) && $story['status'] === 'draft') {
+    if (! $is_test_mode && isset($meta['status']) && $meta['status'] === 'draft') {
         $is_draft = true;
     }
 
@@ -106,18 +116,18 @@ function handlePublish($payload) {
     $content = format_shortcodes($story['content']);
     $content = decode_gutenberg_blocks_html_entities($content);
 
-    $post = array(
-        'post_type'    => $post_type,
-        'post_title'   => $story['title'],
+    $post = [
+        'post_type' => $post_type,
+        'post_title' => $story['title'],
         'post_content' => $content,
         'post_excerpt' => $story['excerpt'] ? $story['excerpt'] : '',
-        'post_status'  => $status,
-        'post_author'  => null,
-        'meta_input'   => array(),
-    );
+        'post_status' => $status,
+        'post_author' => null,
+        'meta_input' => [],
+    ];
 
     // Set the slug
-    if (isset($story['seo_slug']) && !empty($story['seo_slug'])) {
+    if (isset($story['seo_slug']) && ! empty($story['seo_slug'])) {
         $post['post_name'] = $story['seo_slug'];
     }
 
@@ -127,7 +137,7 @@ function handlePublish($payload) {
 
     $post_ID = safely_upsert_story($post);
 
-    $story = array_merge($story, array('external_id' => $post_ID));
+    $story = array_merge($story, ['external_id' => $post_ID]);
 
     // Author
     do_action('storychief_save_author_action', $story);
@@ -155,28 +165,30 @@ function handlePublish($payload) {
     // WPEngine (which caches outside of WP) also listens for this action.
     clean_post_cache($post_ID);
 
-    $permalink = apply_filters( 'storychief_publish_permalink', \Storychief\Tools\getPermalink($post_ID), $post_ID );
+    $permalink = apply_filters('storychief_publish_permalink', \Storychief\Tools\getPermalink($post_ID), $post_ID);
 
-    return array(
+    return [
         'id' => $post_ID,
         'permalink' => $permalink,
         'status' => $is_draft ? 'draft' : 'published',
-    );
+    ];
 }
 
 /**
  * Handle a update webhook call
  *
- * @param array $payload
+ * @param  array  $payload
  * @return array|WP_Error
  *
  * @link https://developers.storychief.io/#ef1fe8bf-6efe-41df-aa7c-f931c128ef61
  */
-function handleUpdate($payload) {
+function handleUpdate($payload)
+{
     $story = $payload['data'];
+    $meta = $payload['meta'];
 
-    if (!get_post_status($story['external_id'])) {
-        return new WP_Error('post_not_found', 'The post could not be found', array('status' => 404));
+    if (! get_post_status($story['external_id'])) {
+        return new WP_Error('post_not_found', 'The post could not be found', ['status' => 404]);
     }
 
     // Before publish action
@@ -185,7 +197,7 @@ function handleUpdate($payload) {
     $is_test_mode = (bool) get_sc_option('test_mode');
     $is_draft = $is_test_mode;
 
-    if (!$is_test_mode && isset($story['status']) && $story['status'] === 'draft') {
+    if (! $is_test_mode && isset($meta['status']) && $meta['status'] === 'draft') {
         $is_draft = true;
     }
 
@@ -195,17 +207,17 @@ function handleUpdate($payload) {
     $content = format_shortcodes($story['content']);
     $content = decode_gutenberg_blocks_html_entities($content);
 
-    $post = array(
-        'ID'           => $story['external_id'],
-        'post_title'   => $story['title'],
+    $post = [
+        'ID' => $story['external_id'],
+        'post_title' => $story['title'],
         'post_content' => $content,
         'post_excerpt' => $story['excerpt'] ? $story['excerpt'] : '',
-        'post_status'  => $status,
-        'meta_input'   => array(),
-    );
+        'post_status' => $status,
+        'meta_input' => [],
+    ];
 
     // Set the slug
-    if (isset($story['seo_slug']) && !empty($story['seo_slug'])) {
+    if (isset($story['seo_slug']) && ! empty($story['seo_slug'])) {
         $post['post_name'] = $story['seo_slug'];
     }
 
@@ -215,7 +227,7 @@ function handleUpdate($payload) {
 
     $post_ID = safely_upsert_story($post);
 
-    $story = array_merge($story, array('external_id' => $post_ID));
+    $story = array_merge($story, ['external_id' => $post_ID]);
 
     // Author
     do_action('storychief_save_author_action', $story);
@@ -243,41 +255,42 @@ function handleUpdate($payload) {
     // WPEngine (which caches outside of WP) also listens for this action.
     clean_post_cache($post_ID);
 
-    $permalink = apply_filters( 'storychief_publish_permalink', \Storychief\Tools\getPermalink($post_ID), $post_ID );
+    $permalink = apply_filters('storychief_publish_permalink', \Storychief\Tools\getPermalink($post_ID), $post_ID);
 
-    return array(
+    return [
         'id' => $post_ID,
         'permalink' => $permalink,
         'status' => $is_draft ? 'draft' : 'published',
-    );
+    ];
 }
 
 /**
  * Handle a delete webhook call
  *
- * @param $payload
  * @return array
  */
-function handleDelete($payload) {
+function handleDelete($payload)
+{
     $story = $payload['data'];
     $post_ID = $story['external_id'];
     wp_delete_post($post_ID);
 
     do_action('storychief_after_delete_action', $story);
 
-    return array(
-        'id'        => $story['external_id'],
+    return [
+        'id' => $story['external_id'],
         'permalink' => null,
-    );
+    ];
 }
 
 /**
  * Handle a connection test webhook call.
  *
- * @param array $payload
+ * @param  array  $payload
  * @return array
  */
-function handleConnectionCheck($payload) {
+function handleConnectionCheck($payload)
+{
     $story = $payload['data'];
 
     do_action('storychief_after_test_action', $story);
@@ -350,21 +363,22 @@ function handleConnectionCheck($payload) {
  *
  * @return mixed
  */
-function missingMethod() {
-    return;
+function missingMethod()
+{
+
 }
 
 /**
  * Safely save a story by disabling & re-enabling sanitation.
  *
- * @param $data
  * @return int
  */
-function safely_upsert_story ($data) {
+function safely_upsert_story($data)
+{
     // disable sanitation
     kses_remove_filters();
 
-    if(isset($data['ID'])) {
+    if (isset($data['ID'])) {
         $post_ID = wp_update_post($data);
     } else {
         $post_ID = wp_insert_post($data);
@@ -376,9 +390,10 @@ function safely_upsert_story ($data) {
     return $post_ID;
 }
 
-function format_shortcodes($content) {
+function format_shortcodes($content)
+{
 
-    preg_match_all('/' . get_shortcode_regex() . '/', $content, $matches, PREG_SET_ORDER);
+    preg_match_all('/'.get_shortcode_regex().'/', $content, $matches, PREG_SET_ORDER);
 
     foreach ($matches as $shortcode) {
         $shortcode_string = $shortcode[0];
@@ -394,13 +409,14 @@ function format_shortcodes($content) {
 /**
  * Replaces html entities that are used in gutenberg blocks.
  */
-function decode_gutenberg_blocks_html_entities($content) {
+function decode_gutenberg_blocks_html_entities($content)
+{
 
     preg_match_all('<!-- wp:(.*?)-->', $content, $matches, PREG_SET_ORDER); // Get all gutenberg blocks
 
-    if (count($matches)){
-        $content = str_replace("&lt;!--", "<!--", $content);
-        $content = str_replace("--&gt;", "-->", $content);
+    if (count($matches)) {
+        $content = str_replace('&lt;!--', '<!--', $content);
+        $content = str_replace('--&gt;', '-->', $content);
         foreach ($matches as $block) {
             $block_json = $block[0];
             if ($block_json) {
@@ -433,7 +449,7 @@ function storychief_debug_mode()
         ini_set('log_errors', 1);
 
         // Sets a log file path you can access in the theme editor.
-        $log_path = STORYCHIEF_DIR . DIRECTORY_SEPARATOR . 'error.log';
+        $log_path = STORYCHIEF_DIR.DIRECTORY_SEPARATOR.'error.log';
         ini_set('error_log', $log_path);
     }
 }
