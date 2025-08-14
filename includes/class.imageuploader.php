d534bf46c7fd94c209157dfa64c3f931944855bf
Gregory Claeyssens <greg@Gregorys-MacBook-Pro.local>
2025-08-14 22:13:37 +0200

    refactor image uploader

--- includes/class.imageuploader.php
+++ includes/class.imageuploader.php
@@ -4,16 +4,17 @@ namespace Storychief;
 
 class ImageUploader
 {
-    public $post;
+    private $post;
+    private $alt;
+    private $storychief_url;
+
     public $url;
-    public $alt;
-    public $filename;
     public $attachment_id;
 
     public function __construct($url, $alt, $post)
     {
         $this->post = $post;
-        $this->url = $url;
+        $this->storychief_url = $url;
         $this->alt = $alt;
     }
 
@@ -27,7 +28,7 @@ class ImageUploader
     {
         $url = $url ?: site_url();
 
-        $urlParts = parse_url($url);
+        $urlParts = wp_parse_url($url);
 
         if (array_key_exists('host', $urlParts) === false) {
             return null;
@@ -47,7 +48,7 @@ class ImageUploader
      */
     public function validate()
     {
-        $host_url = self::getHostUrl($this->url);
+        $host_url = self::getHostUrl($this->storychief_url);
         $site_host_url = self::getHostUrl();
 
         if ($host_url === $site_host_url || !$host_url) {
@@ -58,98 +59,64 @@ class ImageUploader
     }
 
     /**
-     * Return custom image name with user rules
-     * @return string Custom file name
-     */
-    public function getFilename()
-    {
-        $filename = basename($this->url);
-        $this->filename = $filename;
-        return $filename;
-    }
-
-    /**
      * Save image on wp_upload_dir
      * Add image to the media library and attach in the post
      * @return bool
      */
-    public function save()
+    function save()
     {
+        if($attachment = $this->get_attachment_by_storychief_source_url($this->storychief_url)) {
+            $this->url = wp_get_attachment_url( $attachment->ID );
+            $this->attachment_id = $attachment->ID;
+            return true;
+        }
+
+        $get = wp_remote_get( $this->storychief_url );
+
+        $type = wp_remote_retrieve_header( $get, 'content-type' );
+
+        if (!$type || strpos($type, 'image') === false) {
+            return false;
+        }
+
+        $mirror = wp_upload_bits( basename( $this->storychief_url ), null, wp_remote_retrieve_body( $get ) );
+
+        $attachment = array(
+            'post_mime_type' => $type,
+            'post_title'     => $this->alt ?: preg_replace('/\.[^.]+$/', '', basename($this->storychief_url)),
+            'post_content'   => '',
+            'post_status'    => 'inherit',
+        );
+
+        $attach_id = wp_insert_attachment( $attachment, $mirror['file'], $this->post->ID );
+        update_post_meta( $attach_id, '_storychief_source_url', $this->storychief_url );
+
         require_once(ABSPATH . 'wp-admin/includes/image.php');
-        if (function_exists('curl_init') === false) return false;
 
-        setlocale(LC_ALL, "en_US.UTF8");
-        $agent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
-        $ch = curl_init($this->url);
-        curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-        $image_data = curl_exec($ch);
+        $attach_data = wp_generate_attachment_metadata( $attach_id, $mirror['file'] );
 
-        if ($image_data === false) return false;
+        wp_update_attachment_metadata( $attach_id, $attach_data );
 
-        $image_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-        if (strpos($image_type, 'image') === false) {
-            return false;
-        }
-
-        $image_name = $this->getFilename();
-        $upload_dir = wp_upload_dir(date('Y/m', $this->post->post_date ? strtotime($this->post->post_date) : time()));
-        $image_path = urldecode($upload_dir['path'] . '/' . $image_name);
-        $image_url = urldecode($upload_dir['url'] . '/' . $image_name);
-
-        // check if file with same name exists in upload path
-        if (is_file($image_path)) {
-            $this->url = $image_url;
-            // If the image already exists, get the id to link it to the post
-            $attachment_id = attachment_url_to_postid($image_url);
-            if ($attachment_id) {
-                $this->attachment_id = $attachment_id;
-                curl_close($ch);
-                return true;
-            } else {
-                $num = uniqid();
-                $image_path = urldecode($upload_dir['path'] . '/' . $num . '_' . $image_name);
-                $image_url = urldecode($upload_dir['url'] . '/' . $num . '_' . $image_name);
-            }
-        }
-
-        curl_close($ch);
-        file_put_contents($image_path, $image_data);
-
-        if (is_file($image_path) === false) {
-            return false;
-        }
-
-        $this->attachment_id = $this->attachImage($image_path, $image_url, $image_name);
-
-        $this->url = $image_url;
+        $this->attachment_id = $attach_id;
+        $this->url = $mirror['url'];
         return true;
     }
 
-    /**
-     * Attach image to post and media management
-     * @param string $path Image path
-     * @param string $url Image url
-     * @param string $name Image name
-     * @return int $attach_id
-     */
-    public function attachImage($path, $url, $name)
-    {
-        $fileType = wp_check_filetype($path);
-        $attachment = array(
-            'guid'           => $url,
-            'post_mime_type' => $fileType['type'],
-            'post_title'     => $this->alt ?: preg_replace('/\.[^.]+$/', '', $name),
-            'post_content'   => '',
-            'post_status'    => 'inherit'
+    private function get_attachment_by_storychief_source_url( $source_url ) {
+        $args = array(
+            'post_type'   => 'attachment',
+            'post_status' => 'inherit', // attachments usually have this status
+            'meta_query'  => array(
+                array(
+                    'key'   => '_storychief_source_url',
+                    'value' => $source_url,
+                ),
+            ),
+            'posts_per_page' => 1,
         );
-        $attach_id = wp_insert_attachment($attachment, $path, $this->post->ID);
-        $attach_data = wp_generate_attachment_metadata($attach_id, $path);
 
-        wp_update_attachment_metadata($attach_id, $attach_data);
+        $attachments = get_posts( $args );
 
-        return $attach_id;
+        return ! empty( $attachments ) ? $attachments[0] : null;
     }
 }
